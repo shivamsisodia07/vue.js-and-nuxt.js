3a4a3cc86390388c5013e625435f03cb11a59924
shivamsisodia07 <shivamsisodia8656816@gmail.com>
2024-04-24 12:51:02 +0530

    prime vue

---
+++ primevue/primevue/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Button from "primevue/button"
+import Calendar from 'primevue/calendar'
+import InputSwitch from 'primevue/inputswitch'
+import {ref} from 'vue'
+
+const checked=ref(true);
+const date=ref("");
+</script>
+
+<template>
+<h2>hello</h2>
+<Button unstyled :pt="{
+  root:{
+    class:'btn',
+    id:'click'
+  },
+  label:{
+    class:'label'
+  }
+}">click me</Button>
+<Calendar v-model="date"/>
+<InputSwitch v-model="checked"/>
+<h2>{{ checked }}</h2>
+<h2>{{ date }}</h2>
+
+</template>
+
+<style scope>
+
+.label{
+  color: rgb(255, 247, 0);
+}
+.btn{
+  background-color: aqua;
+}
+
+</style>
---
+++ primevue/primevue/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import App from './App.vue'
+
+
+createApp(App).use(PrimeVue).mount('#app');
